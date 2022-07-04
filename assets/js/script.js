4c3872ac74a3e690a9a320da7cb293a54bb144de
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-07-04 16:17:27 -0500

    added dynamic weather info for current day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,60 @@
 var searchBtnEl = document.querySelector("#searchBtn");
 var searchUl = document.querySelector("#search-list");
 var inputEl = document.querySelector("#input");
+var currentEl = document.querySelector("#current-weather");
 
+var getApi = function(cityContent){
+    var city = "oklahoma city";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityContent + "&units=imperial&appid=71b95b62ab18e8bb8a299c550074ab71";
+
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+        response.json().then(function(data){
+
+            var weatherH1 = document.createElement("h1");
+            weatherH1.setAttribute("class", "weather-header");
+            weatherH1.textContent = data.name + " " + "(" + new Date().toLocaleDateString() + ")";
+
+            currentEl.setAttribute("class", "weather");
+
+            var infoUl = document.createElement("ul");
+                infoUl.setAttribute("class", "weather-list");
+            
+            var infoLiTemp = document.createElement("li");
+                infoLiTemp.textContent = "Temp: " + data.main.temp + " F";
+                
+            
+            var infoLiWind = document.createElement("li");
+                infoLiWind.textContent = "Wind: " + data.wind.speed + " mph";
+                
+
+            var infoLiHum = document.createElement("li");
+                infoLiHum.textContent = "Humidity: " + data.main.humidity;
+                
+            
+                var infoLiUv = document.createElement("li");   
+                    infoLiUv.textContent = "UV: 4.7"
+                    
+            
+            infoUl.appendChild(infoLiTemp);
+            infoUl.appendChild(infoLiWind);
+            infoUl.appendChild(infoLiHum);
+            infoUl.appendChild(infoLiUv);
+
+            
+            currentEl.appendChild(weatherH1);
+            currentEl.appendChild(infoUl);
+            
+
+            console.log(data);
+        })
+    } else {
+        currentEl.setAttribute("class", "weather");
+        currentEl.setAttribute("class", "weather-header");
+        currentEl.textContent = "That is not a city we know of, please refresh the page and try again."
+    }
+    })
+}
 
 
 var searchList = function(event){
@@ -12,6 +65,7 @@ var searchList = function(event){
     searchListEl.textContent = cityContent;
 
     searchUl.appendChild(searchListEl);
+    getApi(cityContent);
 };
 
 
