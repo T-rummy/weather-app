354570e4981c354f2f37d4f87bd93f624b8078c1
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-07-03 22:31:17 -0500

    dynamic city buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-console.log("hello");
+var searchBtnEl = document.querySelector("#searchBtn");
+var searchUl = document.querySelector("#search-list");
+var inputEl = document.querySelector("#input");
+
+
+
+var searchList = function(event){
+    event.preventDefault();
+    var searchListEl = document.createElement("button");
+    searchListEl.classList = "list-item col-3 display-block";
+    var cityContent = inputEl.value.trim();
+    searchListEl.textContent = cityContent;
+
+    searchUl.appendChild(searchListEl);
+};
+
+
+searchBtnEl.addEventListener("click", searchList);
